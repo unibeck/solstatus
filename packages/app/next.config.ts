b2bc3d60d1f2edf3eccc6a1b84e2cb196c92bf67
Jonathan Beckman <jonathantbeckman@gmail.com>
2025-06-06 13:45:05 -0400

    Add back nextjs

---
+++ packages/app/next.config.ts
@@ -0,0 +1,50 @@
+import { execSync } from "node:child_process"
+import type { NextConfig } from "next"
+
+// x-release-please-start-version
+const APP_VERSION = "1.6.2"
+// x-release-please-end-version
+
+let gitCommitSHA = "dev"
+console.log(`APP_ENV: ${process.env.APP_ENV}`)
+if (process.env.APP_ENV !== "development") {
+  try {
+    gitCommitSHA = execSync("git rev-parse --short HEAD")
+      .toString()
+      .trim()
+      .substring(0, 7)
+  } catch (error) {
+    console.error("Error getting git commit SHA:", error)
+  }
+}
+const fqAppVersion = `v${APP_VERSION}-${gitCommitSHA}`
+console.log(`NEXT_PUBLIC_APP_VERSION: ${fqAppVersion}`)
+
+const nextConfig: NextConfig = {
+  env: {
+    NEXT_PUBLIC_APP_VERSION: fqAppVersion,
+    NEXT_PUBLIC_APP_ENV: process.env.APP_ENV,
+  },
+  outputFileTracingIncludes: {
+    "/*": ["./registry/**/*"],
+  },
+  experimental: {
+    webpackBuildWorker: true,
+    parallelServerBuildTraces: true,
+    parallelServerCompiles: true,
+    serverSourceMaps: true,
+    typedRoutes: true,
+    reactCompiler: true,
+  },
+}
+
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+})
+
+export default withBundleAnalyzer(nextConfig)
+
+// added by create cloudflare to enable calling `getCloudflareContext()` in `next dev`
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare"
+
+initOpenNextCloudflareForDev()
--- packages/app/types/rw.d.ts
+++
@@ -1,5 +0,0 @@
-import type { AppContext } from "#/worker"
-
-declare module "rwsdk/worker" {
-  interface DefaultAppContext extends AppContext {}
-}
